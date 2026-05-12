b34abef3a4b5b72763d10e02ef0c7d01dcf3c1c3
Drew Farley <old2news@gmail.com>
2026-05-12 03:40:32 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sophisticated Commentary
+I created this NeoVim plugin because I liked the VScode Ctrl+/ comments. With NeoVim, you have to type a few more keys, which is unnacceptable. Therefore, this pluginwas made. Please enjoy
+
+## Installation
+Add this page's url to your vim.pack list.
+Add
+    require('sophisticated-commentary').setup {}
+You're set!
+
+## Usage
+Simply type <Ctrl+/> in any mode to comment/uncomment the current line.<br>
+If you're in visual mode, the currently selected block will be commented/uncommented instead.
---
+++ lua/sophisticated-commentary.lua
@@ -0,0 +1,80 @@
+local Module = {}
+
+function Module.setup(options)
+	options = options or {}
+	local keymap = options.keymap or '<C-_>'
+
+	vim.keymap.set({'n', 'i', 'v'}, keymap, function()
+		-- Supply the adding of comments
+		local cmt = '//'
+		local blockStart = '/*'
+		local blockEnd = '*/'
+		local isBlockComment = false
+		local ft = vim.bo.filetype
+
+		if ft == 'lua' then
+			cmt = '--'
+			blockStart = '--[' .. '['
+			blockEnd = ']]'
+		elseif ft == 'python' then
+			cmt = '#'
+		elseif ft == 'html' then
+			blockStart = '<--'
+			blockEnd = '-->'
+			isBlockComment = true
+		end
+
+		local startRow = vim.fn.line("v") - 1
+		local stopRow = vim.fn.line('.') - 1
+		if stopRow < startRow then
+			startRow, stopRow = stopRow, startRow
+		end
+
+		if stopRow - startRow > 0 and (cmt == '//' or cmt == '--') then
+			isBlockComment = true
+		end
+
+
+		local firstLine = vim.api.nvim_buf_get_lines(0, startRow, startRow + 1, false)[1]
+
+		-- Add comment if any of the selection contains a comment
+		-- Otherwise, remove the comment
+		local addComment = false
+		for line = startRow, stopRow do
+			local currentLine = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
+			if string.find(currentLine, cmt, nil, true) == nil and #currentLine >= #cmt then
+				addComment = true
+				break
+			end
+		end
+
+		-- if addComment and isBlockComment then
+			-- local firstLine = vim.api.nvim_buf_get_lines(0, startRow, startRow + 1, false)[1]
+			-- vim.api.nvim_buf_set_lines(0, startRow, startRow + 1, false, { blockStart .. ' ' .. firstLine })
+			-- local lastLine = vim.api.nvim_buf_get_lines(0, stopRow, stopRow + 1, false)[1]
+			-- vim.api.nvim_buf_set_lines(0, stopRow, stopRow + 1, false, { lastLine .. ' ' .. blockEnd })
+		-- end
+
+		for line = startRow, stopRow do
+			local currentLine = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
+			local indent = #string.match(currentLine, "%s*") + 1
+			if addComment and string.sub(currentLine, indent, indent + #cmt - 1) ~= cmt and #currentLine > 0 then
+				-- The below line makes comment string have the same indent as the text instead of being left-justified
+				local subbed = string.sub(currentLine, 1, indent - 1) .. cmt .. ' ' .. string.sub(currentLine, indent + #cmt - 2)
+				vim.api.nvim_buf_set_lines(0, line, line + 1, false, { subbed })
+			elseif not addComment and string.sub(currentLine, indent, indent + #cmt - 1) == cmt then
+				local subbed = string.sub(currentLine, 1, indent - 1) .. string.sub(currentLine, indent + #cmt + 1)
+				vim.api.nvim_buf_set_lines(0, line, line + 1, false, { subbed })
+			end
+		end
+		vim.api.nvim_feedkeys(vim.keycode'<Esc>', 'n', false)
+		-- vim.api.nvim_buf_set_text(0, start, start, stop, stop, { tostring(start) })
+
+		-- vim.cmd('I-- <ESC>')
+		-- vim.api.nvim_feedkeys("hello", "I", true)
+		-- vim.api.nvim_put({ cmt }, 'c', true, true)
+	end)
+
+end
+
+return Module
