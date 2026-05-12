1dfd8b6efe59b9f67de5681d5b45b68750227dfc
Drew Farley <old2news@gmail.com>
2026-05-12 15:17:05 -0600

    Implemented functions to clean up code

--- lua/sophisticated-commentary.lua
+++ lua/sophisticated-commentary.lua
@@ -58,29 +58,39 @@ function Module.putLine(number, text)
 	vim.api.nvim_buf_set_lines(0, number, number + 1, false, { text })
 end
 
+function Module.getCommentStyle(filetype)
+	-- blockstatus:
+	-- 	0 - Language does not support block comments (python)
+	-- 	1 - Language does support block comments (c, c++, css, javascript, ...)
+	-- 	2 - The only type of comment for the language is block. No inline comments allowed! (html)
+	local cmt = '//'
+	local blockStart = ''
+	local blockEnd = ''
+	local blockStatus = 0
+	if 'lua' == filetype then
+		cmt = '--'
+		blockStart = '--[' .. '['
+		blockEnd = ']]'
+		blockStatus = 1
+	elseif 'python' == filetype then
+		cmt = '#'
+		blockStatus = 0
+	elseif 'html' == filetype then
+		blockStart = '<--'
+		blockEnd = '-->'
+		blockStatus = 2
+	end
+	return cmt, blockStart, blockEnd, blockStatus
+end
+
 function Module.setup(opts)
 	opts = opts or {}
 	local keymap = opts.keymap or '<C-_>'
 
 	vim.keymap.set({'n', 'i', 'v'}, keymap, function()
 		-- Supply the adding of comments
-		local cmt = '//'
-		local blockStart = '/*'
-		local blockEnd = '*/'
-		local isBlockComment = false
-		local ft = vim.bo.filetype
-
-		if ft == 'lua' then
-			cmt = '--'
-			blockStart = '--[' .. '['
-			blockEnd = ']]'
-		elseif ft == 'python' then
-			cmt = '#'
-		elseif ft == 'html' then
-			blockStart = '<--'
-			blockEnd = '-->'
-			isBlockComment = true
-		end
+		local isMultilineComment = false
+		local cmt, blockStart, blockEnd, blockStatus = Module.getCommentStyle(vim.bo.filetype)
 
 		local startRow = vim.fn.line("v") - 1
 		local stopRow = vim.fn.line('.') - 1
@@ -89,45 +99,25 @@ function Module.setup(opts)
 		end
 
 		if stopRow - startRow > 0 and (cmt == '//' or cmt == '--') then
-			isBlockComment = true
+			isMultilineComment = true
 		end
-
-
-		local firstLine = vim.api.nvim_buf_get_lines(0, startRow, startRow + 1, false)[1]
 
 		-- Add comment if any of the selection contains a comment
 		-- Otherwise, remove the comment
 		local addComment = false
 		for line = startRow, stopRow do
-			local currentLine = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
-			if string.find(currentLine, cmt, nil, true) == nil and #currentLine >= #cmt then
+			local currentLine = Module.getLine(line)
+			if not Module.lineHasComment(line, cmt, false) and #currentLine >= #cmt then
 				addComment = true
 				break
 			end
 		end
 
-		-- if addComment and isBlockComment then
-			-- local firstLine = vim.api.nvim_buf_get_lines(0, startRow, startRow + 1, false)[1]
-			-- vim.api.nvim_buf_set_lines(0, startRow, startRow + 1, false, { blockStart .. ' ' .. firstLine })
-			-- local lastLine = vim.api.nvim_buf_get_lines(0, stopRow, stopRow + 1, false)[1]
-			-- vim.api.nvim_buf_set_lines(0, stopRow, stopRow + 1, false, { lastLine .. ' ' .. blockEnd })
-		-- elseif addComment and not isBlockComment then
-			-- local firstLine = vim.api.nvim_buf_get_lines(0, startRow, startRow + 1, false)[1]
-			-- vim.api.nvim_buf_set_lines(0, startRow, startRow + 1, false, { blockStart .. ' ' .. firstLine })
-			-- local lastLine = vim.api.nvim_buf_get_lines(0, stopRow, stopRow + 1, false)[1]
-			-- vim.api.nvim_buf_set_lines(0, stopRow, stopRow + 1, false, { lastLine .. ' ' .. blockEnd })
-		-- end
-
 		for line = startRow, stopRow do
-			local currentLine = vim.api.nvim_buf_get_lines(0, line, line + 1, false)[1]
-			local indent = #string.match(currentLine, "%s*") + 1
-			if addComment and string.sub(currentLine, indent, indent + #cmt - 1) ~= cmt and #currentLine > 0 then
-				-- The below line makes comment string have the same indent as the text instead of being left-justified
-				local subbed = string.sub(currentLine, 1, indent - 1) .. cmt .. ' ' .. string.sub(currentLine, indent + #cmt - 2)
-				vim.api.nvim_buf_set_lines(0, line, line + 1, false, { subbed })
-			elseif not addComment and string.sub(currentLine, indent, indent + #cmt - 1) == cmt then
-				local subbed = string.sub(currentLine, 1, indent - 1) .. string.sub(currentLine, indent + #cmt + 1)
-				vim.api.nvim_buf_set_lines(0, line, line + 1, false, { subbed })
+			if addComment then
+				Module.addComment(line, cmt, false)
+			else
+				Module.removeComment(line, cmt, false)
 			end
 		end
 		-- For exiting visual mode
